894981b89659c7c986fc95161c3a6376da6aaf27
189****8672 <18937359060@189.cn>
2023-12-07 01:51:28 +0000

    增加账号

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
 #PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
-PUSH_TOKEN = ""
+PUSH_TOKEN = "ad20f122e75b42089e74efedaf2126b4"
 
 '''
 *********** 地图配置 ***********
